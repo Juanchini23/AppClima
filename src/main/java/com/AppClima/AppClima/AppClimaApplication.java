31940d05afa52c0b0e4e323c651ca9e7e0be69d8
Juanchini23 <jmanchini@alumno.unlam.edu.ar>
2022-10-13 23:00:39 -0300

    app base

---
+++ src/main/java/com/AppClima/AppClima/AppClimaApplication.java
@@ -0,0 +1,13 @@
+package com.AppClima.AppClima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppClimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppClimaApplication.class, args);
+	}
+
+}
